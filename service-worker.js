b10717c5589bf0f176269f5536e9e74c3537f75d
sihlemnqokoyi <sihlemnqokoyi@gmail.com>
2026-07-16 10:21:30 +0200

    Cache wealth and forecasting module

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='sihle-finance-v2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./bank-import.js','./manifest.json'];
+const CACHE='sihle-finance-v3';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./bank-import.js','./wealth.js','./manifest.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request))));
