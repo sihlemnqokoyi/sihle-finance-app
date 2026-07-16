62534bddf9b6aa7a81949d56c33d41a4bfb75c8e
sihlemnqokoyi <sihlemnqokoyi@gmail.com>
2026-07-16 10:08:03 +0200

    Refresh app cache for statement importer

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE='sihle-finance-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='sihle-finance-v2';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./bank-import.js','./manifest.json'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request))));
