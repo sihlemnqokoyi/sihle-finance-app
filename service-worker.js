d01e201fa8b79985c8ca18dae6a6fe714d0c697e
sihlemnqokoyi <sihlemnqokoyi@gmail.com>
2026-07-16 10:53:33 +0200

    Refresh cache with seeded investment data

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='sihle-finance-v4';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./bank-import.js','./wealth.js','./investment-import.js','./manifest.json'];
+const CACHE='sihle-finance-v5';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./bank-import.js','./wealth.js','./seed-investments.js','./investment-import.js','./manifest.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request))));
